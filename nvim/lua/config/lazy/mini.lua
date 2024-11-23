9e4a1b232fcafd29473575a345f27273d9fb8ac7
Marty Mammel <mmammel12@gmail.com>
2024-11-23 17:32:47 -0600

    add flash and mini.jump

--- nvim/lua/config/lazy/mini.lua
+++ nvim/lua/config/lazy/mini.lua
@@ -1,1 +1,10 @@
-return { 'echasnovski/mini.ai', version = '*' }
+return {
+	{
+		"echasnovski/mini.jump",
+		version = "*",
+		config = function()
+			require("mini.jump").setup()
+		end,
+	},
+	{ "echasnovski/mini.ai", version = "*" },
+}
